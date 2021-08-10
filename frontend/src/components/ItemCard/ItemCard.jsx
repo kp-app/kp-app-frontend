b7bfd35cf3a660816747a019ba876e4c1b2171ab
goldcvt <thegoldcat5@gmail.com>
2021-08-10 17:25:56 +0300

    Simplify card UI

--- frontend/src/components/ItemCard/ItemCard.jsx
+++ frontend/src/components/ItemCard/ItemCard.jsx
@@ -1,19 +1,26 @@
-import { card, category_selector } from './ItemCard.module.css'
-import { OptionsList } from './OptionsList/OptionsList'
+import { card, category_selector, card_container, searchbar_align } from './ItemCard.module.css'
+import { PriceBar } from './PriceBar/PriceBar'
+// import { OptionsList } from './OptionsList/OptionsList'
 import { ProductBar } from './ProductBar/ProductBar'
 
 export const ItemCard = (props) => {
 
     return (
-       <div className={card}>
-           <div className={category_selector}>
-               <select name="category_selector" id="cat_sel">
-                   <option value="value">Коботы</option>
-               </select>
-            </div>
-            <ProductBar />
-            <button>Добавить опцию</button>
-            <OptionsList />
+       <div className={card_container}>
+           <div className={card}>
+               <div className={category_selector}>
+                   <select name="category_selector" id="cat_sel">
+                       <option value="value">Коботы</option>
+                   </select>
+                </div>
+                <div className={searchbar_align}>
+                    <ProductBar />
+                </div>
+                {/* <button>Добавить опцию</button>
+                <OptionsList /> */}
+                
+           </div>
+           <PriceBar/>
        </div>
     )
 }
