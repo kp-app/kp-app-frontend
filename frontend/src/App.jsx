45767921cfd6f4352be0b68eedc1978a7caa6139
goldcvt <thegoldcat5@gmail.com>
2021-08-10 22:11:55 +0300

    Use AddButton

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { ItemCard } from './components/ItemCard/ItemCard'
 import './App.css'
+import { AddButton } from './components/AddButton'
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
       <div className="items">
         <ItemCard />
       </div>
+      <AddButton />
     </div>
   )
 }
