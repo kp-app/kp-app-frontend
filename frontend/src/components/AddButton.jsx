5c4c9832bb82ba1d6137a334dd9f06e1bc59b068
goldcvt <thegoldcat5@gmail.com>
2021-08-10 22:11:41 +0300

    Create button to add items

---
+++ frontend/src/components/AddButton.jsx
@@ -0,0 +1,33 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+    display: flex;
+    width: var(--card-width);
+    margin: auto;
+`
+
+const Button = styled.button`
+    background-color: var(--solid-button-primary-color);
+    border-radius: 3px;
+    color: white;
+    margin: 0.5em 0;
+    padding: 0.25em 1em;
+    width: calc(var(--card-width) / 3);
+    font-size: var(--big-button-font-size);
+    &:hover {
+        border: 2px solid var(--elements-border-color);
+        color: var(--elements-border-color);
+        background: transparent;
+    }
+
+    &:active {
+        border: 2px solid var(--button-click-color);
+        color: var(--button-click-color);
+        background: transparent;
+    }
+`
+
+export const AddButton = (props) => {
+
+    return <Container><Button>Добавить товар в прайс</Button></Container>
+}
