5eb9526e6431abbcb5b90ee51c261a28efe208e8
goldcvt <thegoldcat5@gmail.com>
2021-08-12 00:31:33 +0300

    cleanup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
 import { ItemCard } from './components/ItemCard/ItemCard'
 import './App.css'
 import { AddButton } from './components/AddButton'
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     <div className="App">
