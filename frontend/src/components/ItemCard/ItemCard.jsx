6b8ba6bec488dc95177d52d262d96b60998534bd
goldcvt <thegoldcat5@gmail.com>
2021-08-12 14:11:09 +0300

    add state to ItemCard

--- frontend/src/components/ItemCard/ItemCard.jsx
+++ frontend/src/components/ItemCard/ItemCard.jsx
@@ -1,24 +1,43 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchCategories, fetchItems, pickCategory } from '../../app/itemCardSlice'
+
 import { card, category_selector, card_container, searchbar_align } from './ItemCard.module.css'
 import { PriceBar } from './PriceBar/PriceBar'
 // import { OptionsList } from './OptionsList/OptionsList'
 import { ProductBar } from './ProductBar/ProductBar'
 
 export const ItemCard = (props) => {
+    // useEffect(() => {
+    //     effect
+    //     return () => {
+    //         cleanup
+    //     }
+    // }, [])
+    const dispatch = useDispatch()
+    let currentCategory = useSelector(state => state.itemCard.currentCategory)
+    const items = useSelector(state => state.itemCard.items)
+    const categories = useSelector(state => state.itemCard.categories)
+
+    const categoryChangeHandler = (e) => {
+        let newCategory = e.target.value
+
+        // such usage is not recommended though. We could batch 'em up in reducer or right here w/ some library
+        dispatch(fetchItems(newCategory))
+        dispatch(pickCategory(newCategory))
+    }
 
     return (
        <div className={card_container}>
            <div className={card}>
                <div className={category_selector}>
-                   <select name="category_selector" id="cat_sel">
-                       <option value="value">Коботы</option>
+                   <select name="category_selector" id="cat_sel" onChange={categoryChangeHandler} value={currentCategory}>
+                       {/* TODO Fetch data on load in useEffect */}
+                       {categories.map(category => (<option value={category}>{category}</option>))}
                    </select>
                 </div>
                 <div className={searchbar_align}>
                     <ProductBar />
-                </div>
-                {/* <button>Добавить опцию</button>
-                <OptionsList /> */}
-                
+                </div>          
            </div>
            <PriceBar/>
        </div>
