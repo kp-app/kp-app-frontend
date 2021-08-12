5645fd4311cb1ac8e343e9263813bbab9f392102
goldcvt <thegoldcat5@gmail.com>
2021-08-12 14:10:50 +0300

    Add state to SearchBar, add submit on enter

--- frontend/src/components/ItemCard/ProductBar/ProductBar.jsx
+++ frontend/src/components/ItemCard/ProductBar/ProductBar.jsx
@@ -1,4 +1,6 @@
+import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components"
+import { typeToSearchBar } from "../../../app/itemCardSlice";
 
 const Textarea = styled.input.attrs(props => ({
     type: "text",
@@ -10,7 +12,30 @@ const Textarea = styled.input.attrs(props => ({
     width: var(--search-bar-width);
     font-size: var(--big-button-font-size)
   `;
+const EnterSubmission = styled.input.attrs(props => ({
+  type: "submit"
+}))`
+  border:0;
+  padding:0px;
+  font-size:0.0em;
+`
 
 export const ProductBar = (props) => {
-    return <Textarea size="1.25em" placeholder="Начните искать товар..."></Textarea>
+    const dispatch = useDispatch()
+    let searchbarText = useSelector(state => state.itemCard.currentTextInSearch)
+    
+    const changeInput = (e) => {
+      dispatch(typeToSearchBar(e.target.value))
+    }
+
+    const handleSubmit =(e) => {
+      e.preventDefault()
+    }
+    
+    return (
+      <form onSubmit={handleSubmit}>
+        <Textarea size="1.25em" placeholder="Начните искать товар..." onChange={changeInput} value={searchbarText}></Textarea>
+        <EnterSubmission />
+      </form>
+    )
 }
