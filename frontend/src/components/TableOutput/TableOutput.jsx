6818425cd5ae97966bee6be7ec8c1967cd13cfc3
goldcvt <thegoldcat5@gmail.com>
2021-08-13 15:37:17 +0300

    create TableOutput basic html skeleton

--- frontend/src/components/TableOutput/TableOutput.jsx
+++ frontend/src/components/TableOutput/TableOutput.jsx
@@ -1,3 +1,29 @@
+import { useSelector } from "react-redux"
+
 export const TableOutput = (props) => {
-    return
+    const data = useSelector(state => state.products.addedProducts)
+    const tableData = [(
+        <tr>
+            <td>Номер позиции</td>
+            <td>Оборудование</td>
+            <td>Количество</td>
+            <td>Цена, евро</td>
+        </tr>
+    )].concat( 
+    data.map((entry, index) => {
+        return (
+            <tr>
+                <td>{index + 1}</td>
+                <td>{entry.fullName}</td>
+                <td>{entry.quantity}</td>
+                <td>{entry.price * entry.quantity}</td>
+            </tr>
+        )
+    }))
+    return (
+        <div>
+            <table>{tableData}</table>
+        </div>
+    )
 }
+
