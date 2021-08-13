b5bcfc08deee6737eeafd0d3ffe0f5390caa0d18
goldcvt <thegoldcat5@gmail.com>
2021-08-13 11:36:55 +0300

    get rid of redundant data structures in store

--- frontend/src/app/productsSlice.js
+++ frontend/src/app/productsSlice.js
@@ -2,12 +2,7 @@ import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
     addedProducts: [
-    ],
-    priceTable: {
-        itemNames: [],
-        prices: [],
-        costs: []
-    }
+    ]
 }
 
 const productsSlice = createSlice(
@@ -20,23 +15,28 @@ const productsSlice = createSlice(
                 if (lastProductEntry === -1) {
                     let item = {...action.payload, quantity: 1}
                     state.addedProducts = [...state.addedProducts, item]
-                    state.priceTable.itemNames = [...state.priceTable.itemNames, item.fullName]
-                    // TODO what if we wanna send this proposal to partner, not end customer?
-                    state.priceTable.prices = [...state.priceTable.prices, item.price]
-                    state.priceTable.costs = [...state.priceTable.prices, item.cost]
                 } else {
-                    state.addedProducts[lastProductEntry] = {...state.addedProducts[lastProductEntry], quantity: state.addedProducts[lastProductEntry] + 1}
+
+                    state.addedProducts[lastProductEntry] = {...state.addedProducts[lastProductEntry], quantity: state.addedProducts[lastProductEntry].quantity + 1}
+                    
                 }
             },
             removeItem(state, action) {
                 
                 let lastProductEntry = state.addedProducts.map(product => product.fullName).lastIndexOf(action.payload.fullName)
                 if (lastProductEntry !== -1) {
-                    // it's ok to mutate since nothing mutates aremove(action.payload)nyway :)
-                    state.addedProducts.splice(lastProductEntry, 1)
-                    state.priceTable.itemNames.splice(lastProductEntry, 1)
-                    state.priceTable.prices.splice(lastProductEntry, 1)
-                    state.priceTable.costs.splice(lastProductEntry, 1)
+                    if (state.addedProducts[lastProductEntry].quantity > 1) {
+            
+                        // Reduce quantity, reduce cost & price
+                        state.addedProducts[lastProductEntry] = {
+                            ...state.addedProducts[lastProductEntry], 
+                            quantity: state.addedProducts[lastProductEntry].quantity - 1
+                        }
+                        
+                    } else {
+                        state.addedProducts.splice(lastProductEntry, 1)
+
+                    }
                 } else {
                     throw {message: "Tried to remove non-existing element :)", name: "No such value to remove"}
                 }
