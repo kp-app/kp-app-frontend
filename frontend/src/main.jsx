cd6b2558cc22afe802f69084cb10f61ef1fb7ac0
goldcvt <thegoldcat5@gmail.com>
2021-08-20 17:58:25 +0300

    add sticky header

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,24 @@
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import { Provider } from 'react-redux'
+import {Provider} from 'react-redux'
 import store from './app/store'
+import styled from "styled-components";
+
+const StickyHeader = styled.header`
+  display: block;
+  position: sticky;
+  top: 0;
+  width: 100%;
+  background-color: var(--solid-button-primary-color);
+  height: 80px;
+  margin-bottom: 10px;
+`
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <StickyHeader/>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
 )
