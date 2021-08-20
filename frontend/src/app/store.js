fc951a7317c9e47299271ec28078c74d94ec344a
goldcvt <thegoldcat5@gmail.com>
2021-08-20 17:58:51 +0300

    add new slice to store

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,13 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit'
+import {configureStore} from '@reduxjs/toolkit'
 import itemCardSlice from './itemCardSlice'
 import productsSlice from './productsSlice'
-
-
+import authSlice from "./authSlice";
 
 
 export default configureStore({
-  reducer: {
-      itemCard: itemCardSlice,
-      products: productsSlice
-  },
+    reducer: {
+        itemCard: itemCardSlice,
+        products: productsSlice,
+        auth: authSlice
+    },
 })
