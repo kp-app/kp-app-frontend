baf1147e44d8b50f02062acabb49d27d10953b2a
goldcvt <thegoldcat5@gmail.com>
2021-08-20 17:59:30 +0300

    Add auth to display logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,49 @@
-import { ItemCard } from './components/ItemCard/ItemCard'
+import {ItemCard} from './components/ItemCard/ItemCard'
 import './App.css'
-import { CreateTableButton } from './components/SwitchToTableButton'
-import { AddedItems } from './components/AddedItems/AddedItems'
-import { Total } from './components/Total'
-import { useSelector } from 'react-redux'
-import { TableOutput } from './components/TableOutput/TableOutput'
+import {CreateTableButton} from './components/SwitchToTableButton'
+import {AddedItems} from './components/AddedItems/AddedItems'
+import {Total} from './components/Total'
+import {useSelector} from 'react-redux'
+import {TableOutput} from './components/TableOutput/TableOutput'
+import styled from "styled-components";
+
+
+const Items = (props) => {
+    const isCards = useSelector(state => state.products.tableView)
+    return (
+        <div>
+            <div className="items">
+                <ItemCard/>
+            </div>
+            <CreateTableButton/>
+            {!isCards && <AddedCards/>}
+            {isCards && <TableOutput/>}
+        </div>
+    )
+}
+
+const TRedWrapper = styled.div``
 
 const AddedCards = (props) => {
-  return (
-  <div className="added_items">
-    <Total />
-    <AddedItems />
-  </div>
-)}
+    return (
+        <div className="added_items">
+            <Total/>
+            <AddedItems/>
+        </div>
+    )
+}
 
 function App() {
-  const isCards = useSelector(state => state.products.tableView)
+    const isAuthenticated = useSelector(state => state.auth.token)
 
 
-  return (
-    <div className="App">
-      <div className="items">
-        <ItemCard />
-      </div>
-      <CreateTableButton />
-      {!isCards && <AddedCards />}
-      {isCards && <TableOutput />}
-    </div>
-  )
+    return (
+        <div className="App">
+            {isAuthenticated && <Items/>}
+            {!isAuthenticated && <TRedWrapper/>}
+        </div>
+    )
+
 }
 
 export default App
