edad47c4da6e3b74538574b3baa4b7232b452637
goldcvt <thegoldcat5@gmail.com>
2021-08-20 16:41:38 +0300

    create authSlice

---
+++ frontend/src/app/authSlice.js
@@ -0,0 +1,40 @@
+import {createSlice} from "@reduxjs/toolkit"
+import {backendUrl} from "../../backendData"
+
+const initialState = {
+    currentCredentials: {
+        username: "",
+        password: ""
+    },
+    token: ""
+}
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        async login(state) {
+            const token = await fetch(`${backendUrl}auth/login`, {
+                body: JSON.stringify({...state.currentCredentials})
+            })
+            state.token = token
+            state.currentCredentials.username = ""
+            state.currentCredentials.password = ""
+        },
+
+        typeUsername(state, action) {
+            state.currentCredentials.username = action.payload
+        },
+
+        typePassword(state, action) {
+            state.currentCredentials.password = action.payload
+        },
+
+        logout(state) {
+            state.token = ""
+        }
+    }
+})
+
+export const {login, typeUsername, typePassword, logout} = authSlice.actions
+export default authSlice.reducer
