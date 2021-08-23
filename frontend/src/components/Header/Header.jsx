3e3212035bee03b30462435e8d5a545761ed2ab2
goldcvt <thegoldcat5@gmail.com>
2021-08-23 11:56:54 +0300

    create prettier form, add statefulness

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import {useDispatch, useSelector} from "react-redux";
 import {Popup} from "../reusables/PopupWindow";
-import {togglePopup, typePassword, typeUsername} from "../../app/authSlice";
+import {clearCredentials, login, togglePopup, typePassword, typeUsername, logout} from "../../app/authSlice";
 
 const StickyHeader = styled.header`
   display: flex;
@@ -20,11 +20,36 @@ const AuthButton = styled.button`
   margin-top: 0;
   margin-bottom: 0;
   margin-right: 60px;
-  height: 50px;
+  height: 60px;
   background-color: var(--secondary-color);
   color: black;
   font-size: var(--big-button-font-size);
+    display: flex;
+    justify-content: center;
+    align-items: center
+`
 
+const InputsDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+`
+
+const Input = styled.input`
+    width: 500px;
+    height: 40px;
+    font-size: var(--small-button-font-size);
+    border-radius: 12px;
+
+`
+
+const SubmitLoginButton = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 40px;
+    font-size: var(--small-button-font-size);
+    margin-bottom: 0px;
 `
 
 const AuthForm = props => {
@@ -37,12 +62,22 @@ const AuthForm = props => {
         dispatch(typePassword(e.target.value))
     }
 
+    
     return (
-        <form>
-            <input type={'text'} value={props.credentials.username} onChange={writeUsername}/>
-            <input type={'password'} value={props.credentials.password} onChange={writePassword}/>
+        <form onSubmit={async (e) => {
+            e.preventDefault()
+            const thing = await dispatch(login(props.credentials))
+            console.log(thing)
+        }}>
+            <InputsDiv>
+                <Input type={'text'} value={props.credentials.username} onChange={writeUsername}/>
+                <Input type={'password'} value={props.credentials.password} onChange={writePassword}/>
+                <SubmitLoginButton type={'submit'}>Войти</SubmitLoginButton>
+            </InputsDiv>
+
         </form>
     )
+    
 }
 
 export const Header = () => {
@@ -53,9 +88,13 @@ export const Header = () => {
         <StickyHeader>
             <AuthButton
                 onClick={() => {
-                    dispatch(togglePopup())
+                    const clear = () => {
+                        dispatch(clearCredentials())
+                        dispatch(logout())
+                    }
+                    authState.token ? clear() : dispatch(togglePopup())
                 }}>{!authState.token ? "Войти" : `${authState.user} (Выйти)`}</AuthButton>
-            {authState.popupOpen &&
+            {authState.popupOpen && !authState.token &&
             <Popup isActive={authState.popupOpen} action={togglePopup}><AuthForm credentials={credentials}/></Popup>}
         </StickyHeader>
     )
