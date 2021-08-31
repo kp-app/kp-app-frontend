7b133d4d9cd8339192e8331b70857d484d7f3929
goldcvt <thegoldcat5@gmail.com>
2021-08-31 12:04:48 +0300

    rework reducers in itemCardSLice

--- frontend/src/app/itemCardSlice.js
+++ frontend/src/app/itemCardSlice.js
@@ -1,52 +1,107 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import axios from "axios"
+import { backendUrl } from "../../backendConfig"
 import { searchEveryWord } from "../utils/searchEveryWord"
 
 // create init state
 const initialState = {
-    categories: ["Industrial Robots Universal Dummy", "Cobots Universal Dummy"],
-    currentCategory: "Cobots Universal Dummy",
+    categories: [],
+    currentCategory: {},
+    subcategories: [],
+    currentSubcategory: {},
     currentTextInSearch: "",
     searchPredictions: [],
     currentItem: {},
-    items: [
-        {
-            category: "Cobots Universal Dummy",
-            fullName: "Dummy Bot 7iA/L AB",
-            itemModel: "Dummy Bot 7iA/L",
-            cost: 77000,
-            price: 90010
-        },
-        {   
-            category: "Cobots Universal Dummy",
-            fullName: "Cool Bot 777 iA/L A",
-            itemModel: "Cool Bot 777 iA/L",
-            cost: 677000,
-            price: 690010,
-        },
-        {
-            category: "Cobots Universal Dummy",
-            fullName: "Cool Bot CX iA/L A",
-            itemModel: "Cool Bot CX iA/L",
-            cost: 977000,
-            price: 990010,
-        }
-    ]
+    items: []
 }
 
+export const loadCategories = createAsyncThunk(
+    'itemCard/loadCategories',
+    async (data, {fulfillWithValue, rejectWithValue, dispatch}) => {
+        try {
+            let {token} = data
+            const config = {
+                headers: { Authorization: `Bearer ${token}` }
+            }
+            const response = await axios.get(
+                `${backendUrl}categories`,
+                config
+            )
+            if (response.status !== 200) {
+                throw new Error('Failed to return data from API')
+            }
+            dispatch(clearUpToCategories()) // kind of a side-effect, right?
+            return fulfillWithValue(response.data)
+        } catch (e) {
+            return rejectWithValue(e.message)
+        }
+    }
+)
+
+export const loadSubcategories = createAsyncThunk(
+    'itemCard/loadSubcategories',
+    async (data, {fulfillWithValue, rejectWithValue, dispatch}) => {
+        try {
+            let {token, category} = data
+            if (Object.keys(category).length !== 0) {
+                const config = {
+                    headers: { Authorization: `Bearer ${token}` }
+                };
+                const response = await axios.get(
+                    // TODO use proper params pls
+                    `${backendUrl}subcategories?categoryId=${category._id}`,
+                    config
+                )
+                // console.log(response)
+                if (response.status !== 200 && response.status !== 201) {
+                    throw new Error('Failed to return data from API')
+                }
+                dispatch(clearItems()) // kind of a side-effect, right?
+                return fulfillWithValue(response.data)
+            }
+        } catch (e) {
+            return rejectWithValue(e.message)
+        }
+    }
+)
+
+export const loadItems = createAsyncThunk(
+    'itemCard/loadItems',
+    async (data, {fulfillWithValue, rejectWithValue, dispatch}) => {
+        try {
+            let {token, subcategory} = data
+            if (Object.keys(subcategory).length !== 0) {
+                const config = {
+                    headers: { Authorization: `Bearer ${token}` }
+                };
+                const response = await axios.get(
+                    // TODO use proper params pls
+                    `${backendUrl}products/subcategory?subcategoryId=${subcategory._id}`,
+                    config
+                )
+                console.log(response)
+                if (response.status !== 200) {
+                    throw new Error('Failed to return data from API')
+                }
+                return fulfillWithValue(response.data)
+            } else {
+                console.log("Broken subcat obj", subcategory)
+            }
+        } catch (e) {
+            return rejectWithValue(e.message)
+        }
+    }
+)
 // write slice
 const itemCardSlice = createSlice({
     name: "itemCard",
     initialState,
     reducers: {
-        // categories
-        fetchCategories(state) {
-            // idk man, it has something to do with fetching. asyncThunk bs?
-        },
-        fetchItems(state, payload) {
-            // payload = category.value
-        },
         pickCategory(state, action) {
-            state.currentCategory = action.payload
+            state.currentCategory = state.categories.filter(cat => cat.name === action.payload)[0]
+        },
+        pickSubcategory(state, action) {
+            state.currentSubcategory = state.subcategories.filter(subcat => subcat.name === action.payload)[0]
         },
         // searchBar text/predictions
         typeToSearchBar(state, action) {
@@ -70,22 +125,52 @@ const itemCardSlice = createSlice({
            }
 
         },
-        submitItem(state) {
-            // submit state.currentItem
-            state.currentItem = {}
-        },
-        clearItem(state) {
-            state.currentItem = {}
-        },
         clearSearchBar(state) {
             state.currentTextInSearch = ""
             state.searchPredictions = []
             state.currentItem = {}
         },
-
+        clearUpToCategories(state) {
+            state.subcategories = []
+            state.items = []
+            state.currentItem = {}
+            state.currentTextInSearch = ""
+            state.searchPredictions = []
+        },
+        clearItems(state) {
+            state.items = []
+            state.currentItem = {}
+            state.currentTextInSearch = ""
+            state.searchPredictions = []
+        }
+    },
+    extraReducers: {
+        [loadCategories.fulfilled]: (state, action) => {
+            state.categories = action.payload
+        },
+        [loadSubcategories.fulfilled]: (state, action) => {
+            state.subcategories = action.payload
+        },
+        [loadItems.fulfilled]: (state, action) => {
+            state.items = action.payload
+        },
+        [loadCategories.rejected]: (state, action) => {
+            console.log(action.payload)
+        },
+        [loadSubcategories.rejected]: (state, action) => {
+            console.log(action.payload)
+        },
+        [loadItems.rejected]: (state, action) => {
+            console.log(action.payload)
+        }
     }
 })
 
-export const { fetchCategories, fetchItems, pickCategory, typeToSearchBar, selectItem, submitItem, clearItem, clearSearchBar } = itemCardSlice.actions
+export const { 
+    pickCategory, typeToSearchBar, 
+    selectItem, clearSearchBar, 
+    clearUpToCategories, clearItems,
+    pickSubcategory
+} = itemCardSlice.actions
 
 export default itemCardSlice.reducer
