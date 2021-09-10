64888b14b171af00f9e3e6406f6a2daf5486c503
goldcvt <thegoldcat5@gmail.com>
2021-09-10 17:24:48 +0300

    create item display logic

---
+++ frontend/src/antd/components/ItemGrid/ItemsBySubcategory.jsx
@@ -0,0 +1,30 @@
+import { useEffect } from "react"
+import { useSelector } from "react-redux"
+import { useDispatch } from "react-redux"
+import { loadItemsBySubcategory } from "../../../app/itemCardSlice"
+import {Card} from 'antd'
+import { AddButton } from "./AddToCartButton"
+
+export const ItemsBySubcat = (props) => {
+    const {token, subcategory} = props
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(loadItemsBySubcategory({token: token, subcategory: subcategory}))
+    }, [])
+    let items = useSelector(state => state.itemCard.items).filter(itemGroup => itemGroup.subcategory.name === subcategory.name)
+    items.length > 0 ? items = items[0].items : null
+    return (
+        <div style={{
+                display: "grid", 
+                gridTemplateColumns: "repeat(auto-fill, minmax(200px, 5fr))"
+            }}>
+                {items.map(item => <Card title={null} bordered={true}>
+                    <div style={{display: "flex", flexDirection: "column", justifyContent: "space-between", alignItems: "center"}}>
+                        <div>{item.fullName}</div>
+                            <AddButton item={item}/>
+                        <div>Стоимость {item.pricing.pricelistCost !== 0 ? item.pricing.pricelistCost : "недоступна"}</div>
+                    </div>
+                </Card>)}
+        </div>
+    )
+}
---
+++ frontend/src/antd/components/Layout/ItemGrid.jsx
@@ -0,0 +1,29 @@
+import {useDispatch, useSelector} from "react-redux";
+import { useLocation } from "react-router-dom";
+import { useEffect } from 'react';
+import { loadSubcategories, loadItemsByCategory, clearUpToCategories } from "../../../app/itemCardSlice";
+import { ItemsBySubcat } from "../ItemGrid/ItemsBySubcategory";
+
+export const ItemGrid = (props) => {
+    // filters
+    const categoryId = useLocation().pathname.split("/").slice(-1)[0];
+    const token = useSelector(state => state.auth.token)
+    const dispatch = useDispatch()
+    // load subcats, items by cat
+    useEffect(() => {
+        dispatch(clearUpToCategories())
+        dispatch(loadSubcategories({token: token, categoryId: categoryId}))
+    }, [categoryId])
+
+    const subcategories = useSelector(state => state.itemCard.subcategories)
+    // display grid
+    return (
+        <>
+            {subcategories.map((subcat, index) => <div style={{width: 1200}}>
+                <h1>{subcat.name}</h1>
+                <ItemsBySubcat token={token} subcategory={subcat}/>
+            </div>)}
+        </>
+        // filter applied after .map() as .filter(subcat == activeSubcats) 
+    )
+}
