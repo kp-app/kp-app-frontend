c1ce499a18bfbd91327c7f4e509258a0cdafdf40
goldcvt <thegoldcat5@gmail.com>
2021-09-10 12:02:59 +0300

    add cart, products paths

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,20 +8,32 @@ import store from './app/store'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import {AuthedHeader} from "./antd/AuthedLayout";
 import AdminApp from "./antd/AdminApp";
+import { ItemGrid } from './antd/components/Layout/ItemGrid'
+import { CartLayout } from './antd/components/Cart/CartLayout'
 
 
 ReactDOM.render(
     <BrowserRouter>
         <Provider store={store}>
             <AuthedHeader>
-                <Switch>
-                    <Route exact path='/'>
-                        <App/>
-                    </Route>
-                    <Route exact path='/admin'>
-                        <AdminApp/>
-                    </Route>
-                </Switch>
+                {/* Hey, I'm unironically dead inside. This app is a fucking joke fr fr */}
+                
+                <Route exact path='/'>
+                    <App/>
+                </Route>
+                <Route exact path='/admin'>
+                    <AdminApp/>
+                </Route>
+                <Route path='/category/:categoryId'>
+                    <App>
+                        <ItemGrid />
+                    </App>
+                </Route>
+                <Route path='/cart'>
+                    <App>
+                        <CartLayout />
+                    </App>
+                </Route>
             </AuthedHeader>
         </Provider>
     </BrowserRouter>,
