9d63a47531747091f0420c55e002ab6c54afd496
goldcvt <thegoldcat5@gmail.com>
2021-09-10 17:23:46 +0300

    Simplify catalog

--- frontend/src/antd/components/CatalogContent/Catalog.jsx
+++ frontend/src/antd/components/CatalogContent/Catalog.jsx
@@ -1,46 +1,26 @@
 import {useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {Layout, Menu} from "antd";
+import { clearUpToCategories, loadCategories } from "../../../app/itemCardSlice";
+import { NavLink } from "react-router-dom";
 
 export const CatalogContent = (props) => {
-    const {Header, Content, Sider} = Layout;
-
+    const token = useSelector(state => state.auth.token)
     // download categories + respective subcategories into state on load
     // I mean, default shi', can't figure out nothing more elegant
     const dispatch = useDispatch()
     useEffect(() => {
+        dispatch(loadCategories({token: token}))
     }, [])
-
-    const subcats = useSelector(state => state.itemCard.subcategories)
     const cats = useSelector(state => state.itemCard.categories)
     return (
-        <Layout>
-            <Sider>
-                <Menu
-                    mode="inline"
-                    style={{height: '100%', borderRight: 0}}
-                >
-                    {cats.map(category => (<Menu.Item key={category._id}>{category.name}</Menu.Item>))}
-                </Menu>
-            </Sider>
-            <Layout style={{padding: '0 24px 24px', minWidth: '1000px'}}>
-                <Content
-                    style={{
-                        padding: 24,
-                        margin: 0,
-                        minHeight: 280,
-                    }}
-                >
-                    <div className="site-layout">
-                        <Menu mode="inline"
-                              style={{height: '100%', borderRight: 0}}>
-                            {subcats.map(subcategory => (<Menu.Item key={subcategory._id}>
-                                {subcategory.name}
-                            </Menu.Item>))}
-                        </Menu>
-                    </div>
-                </Content>
-            </Layout>
-        </Layout>
+         
+        <Menu
+            mode="inline"
+            style={{height: '100%', borderRight: 0}}
+        >
+            {cats.map(category => (<Menu.Item key={category._id}><NavLink to={`/category/${category._id}`}>{category.name}</NavLink></Menu.Item>))}
+        </Menu>
+        
     )
 }
